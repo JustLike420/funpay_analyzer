1f9355fd34b732ec30bf030ead2347597e2a72f0
Vladimir <justlikevova@gmail.com>
2022-11-05 20:30:21 +0300

    modify models

--- analyzer/models.py
+++ analyzer/models.py
@@ -3,9 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Seller(models.Model):
-    username = models.CharField(max_length=100, verbose_name='Ник')
+    username = models.CharField(max_length=100, verbose_name='Ник', unique=True)
     user_url = models.URLField(verbose_name='Сылка на профиль')
     rating = models.IntegerField(verbose_name='Отзывы')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Продовец'
+        verbose_name_plural = 'Продовцы'
 
 
 class Offer(models.Model):
@@ -18,3 +25,10 @@ class Offer(models.Model):
     href = models.URLField(verbose_name='Сылка на предложение')
     count = models.IntegerField(verbose_name='Количество')
     price = models.FloatField(verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
