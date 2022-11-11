a7a6c65874105ad9a8924371b3075963a89ef9f0
Vladimir <justlikevova@gmail.com>
2022-11-11 22:15:48 +0300

    custom admin
    filter by price range
    filter by type

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from rangefilter.filters import NumericRangeFilter
+
 from .models import Seller, Offer
+
+
 # Register your models here.
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+    list_filter = (('price', NumericRangeFilter), 'type')
+
+
 admin.site.register(Seller)
-admin.site.register(Offer)
